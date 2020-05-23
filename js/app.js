e2ce62b736924fac75eabe7feb764bb33a56b1d6
Toni Peise <toni.peise@gmx.net>
2020-05-23 19:09:50 +0200

    1.5 sidenav added

--- js/app.js
+++ js/app.js
@@ -3,3 +3,21 @@ $("#slideshow > div:gt(0)").hide();
 setInterval(function () {
    $("#slideshow > div:first").fadeOut(500).next().fadeIn(500).end().appendTo("#slideshow");
 }, 3000);
+
+$('a[href^="#"]').on("click", function (e) {
+   e.preventDefault();
+   var target = this.hash;
+   var $target = $(target);
+   $("html, body")
+      .stop()
+      .animate(
+         {
+            scrollTop: $target.offset().top,
+         },
+         900,
+         "swing",
+         function () {
+            window.location.hash = target;
+         }
+      );
+});
